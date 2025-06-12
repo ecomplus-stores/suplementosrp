9154fb762568178126fe3cfb02abdbd036d87bdb
Vitor Rocha Gonçalves <35348281+vitorrgg@users.noreply.github.com>
2025-06-12 15:44:01 -0300

    Update storefront.webpack.js

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -4,7 +4,7 @@ const dirSearchAlias = path.resolve(__dirname, 'template/js/lib/search-engine')
 module.exports = () => ({
   resolve: {
     alias: {
-      './methods/set-search-term': path.resolve(dirSearchAlias, 'set-search-term')
+      // './methods/set-search-term': path.resolve(dirSearchAlias, 'set-search-term')
     }
   }
 })
